737beb22354224936b25754ff6e06f620174d333
Harmedino <adamsahmed191@gmail.com>
2023-10-24 14:40:26 +0100

    calculator almost done

--- level 1 calculator/script.js
+++ level 1 calculator/script.js
@@ -1,3 +1,4 @@
+let confirm = true
 // Get the display element
 const screen = document.getElementById('display')
 
@@ -16,6 +17,11 @@ const equal = document.getElementById('equal')
 // Event listeners for number buttons
 buttons.forEach(button => {
     button.addEventListener('click', function (event) {
+        confirm = true
+        if (screen.value == 0 ) {
+            screen.value = '';
+        }
+        
         // Append the clicked button's text to the display
         screen.value += event.target.textContent;
     });
@@ -24,14 +30,26 @@ buttons.forEach(button => {
 // Event listener for the clear button
 clear.addEventListener('click', () => {
     // Clear the display
-    screen.value = '';
+    screen.value = 0;
 });
 
 // Event listeners for operation buttons
+
 operations.forEach(opera => {
     opera.addEventListener('click', function (event) {
+        if (confirm) { 
+            screen.value += event.target.textContent;
+        } 
+        const lastCharacter = screen.value[screen.value.length - 1];
+        if (event.target.textContent == lastCharacter) {
+           
+           confirm = false
+        } else {
+
+            screen.value = screen.value.substring(0, screen.value.length - 1) + event.target.textContent;
+        }
+       
         // Append the clicked operation button's text to the display
-        screen.value += event.target.textContent;
     });
 });
 
